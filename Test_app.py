0abfbac22ca771607aea6124f908a37779b2df31
Rajneet Kaur <rajneet.mann@gmail.com>
2024-11-08 09:55:53 +0100

    Add Python app, test, and workflow

---
+++ Test_app.py
@@ -0,0 +1,5 @@
+# test_app.py
+from app import add_one_plus_one
+
+def test_add_one_plus_one():
+    assert add_one_plus_one() == 2
---
+++ app.py
@@ -0,0 +1,3 @@
+# app.py
+def add_one_plus_one():
+    return 1 + 1
